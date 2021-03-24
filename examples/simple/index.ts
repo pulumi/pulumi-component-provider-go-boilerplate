8b920fcf31df88c8083737367aaecac386153c41
Luke Hoban <luke@pulumi.com>
2021-03-24 15:39:41 +1100

    Full example

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,9 +1,15 @@
 import * as apigateway from "@pulumi/apigateway";
 import * as aws from "@pulumi/aws";
 
-const f = new aws.lambda.Function("f", {
-
-});
+const f = new aws.lambda.CallbackFunction("f", {
+    callback: async (ev, ctx) => {
+        console.log(JSON.stringify(ev));
+        return {
+            statusCode: 200,
+            body: "hello",
+        };
+    },
+})
 
 const api = new apigateway.RestAPI("api", {
     routes: [{
